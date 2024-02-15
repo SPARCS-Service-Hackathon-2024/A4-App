3e9410888eb72165926ad1f32bf6f803dda405b0
thornappl2 <thornappl2@gmail.com>
2024-02-16 04:28:24 +0900

    feat: 페이지2 우리 동네 게시판 구현 #1

--- app_front/lib/main.dart
+++ app_front/lib/main.dart
@@ -3,34 +3,64 @@
 import 'package:app_front/View/Screen/login_page.dart';
 
 import 'package:flutter/material.dart';
+import 'package:app_front/View/Screen/RentalTap.dart';
+import 'package:app_front/View/Screen/RentalPage.dart';
+import 'package:app_front/View/Screen/BulletinBoardPage.dart';
 
-
-
-void main(){
-
-  runApp(const MyApp());
+void main() {
+  runApp(MyApp());
 }
 
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      title: '로그인 앱',
+    return MaterialApp(
+      title: 'My App',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
-
+      home: HomePage(),
     );
   }
+}
 
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(16.0),
+            color: Colors.grey[200],
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RentalPage()),
+                );
+              },
+              child: Text('대여창고'),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(16.0),
+            color: Colors.grey[200],
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => BulletinBoardPage()),
+                );
+              },
+              child: Text('우리동네 게시판'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
